ef422f24a7a0feee34b48f29c9b8e8fa8124cc82
residentevilfreak <112755412+residentevilfreak@users.noreply.github.com>
2025-10-12 01:50:56 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,130 +1,63 @@
 #!/bin/bash
-
-# exit immediately if a command exits with a non-zero status.
 set -e
 
-# --- pacman packages ---
-echo "--- installing pacman packages ---"
+echo "=== arch linux setup script ==="
 
-# list of packages
-pacman_packages=(
-    # essentials
-    amd-ucode
-    base-devel
-    btop
-    dnsmasq
-    iwd
-    hostapd
-    openssh
-    pacman-contrib
-    power-profiles-daemon
-    reflector
-    smartmontools
-    os-prober
-    git
-    wget
-    xdg-utils
-
-    # utilities
-    plasma-meta
-    ark
-    dolphin
-    kate
-    kio-admin
-    konsole
-    packagekit-qt6
-    noto-fonts
-    noto-fonts-emoji
-    
-    # terminal
-    fastfetch
-    nano
-    zsh
-    zsh-autosuggestions
-
-    # file management 
-    unrar
-
-    # applications
-    firefox
-    mpv
-    gwenview
-    filelight
-    filezilla
-    obs-studio
-    qbittorrent
-    bitwarden
-
-    # gaming
-    steam
-    lutris
-    wine
-    mangohud
-    gamemode
-    nvidia-settings
+# pacman packages
+PACMAN_PKGS=(
+  amd-ucode base-devel btop dnsmasq iwd hostapd openssh pacman-contrib
+  power-profiles-daemon reflector smartmontools os-prober git wget xdg-utils
+  plasma-meta ark dolphin kate kio-admin konsole packagekit-qt6 noto-fonts
+  noto-fonts-emoji fastfetch nano fish unrar firefox mpv
+  gwenview filelight filezilla obs-studio qbittorrent bitwarden steam lutris
+  wine mangohud gamemode nvidia-settings
 )
 
-# install packages. --needed will not reinstall packages that are already up to date.
-# pacman's output will tell which ones are being skipped.
-sudo pacman -S --needed --noconfirm "${pacman_packages[@]}"
+# check which packages are already installed
+echo -e "\n=== checking installed packages ==="
+for pkg in "${PACMAN_PKGS[@]}"; do
+  if pacman -Q "$pkg" &>/dev/null; then
+    echo "✓ $pkg (already installed)"
+  fi
+done
 
+# install pacman packages
+echo -e "\n=== installing pacman packages ==="
+sudo pacman -S --noconfirm "${PACMAN_PKGS[@]}"
 
-# --- install paru ---
-if ! command -v paru &> /dev/null; then
-    echo "--- installing paru ---"
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
-    makepkg -si --noconfirm
-    cd ..
-    rm -rf paru
-else
-    echo "--- paru is already installed ---"
+# install paru
+if ! command -v paru &>/dev/null; then
+  echo -e "\n=== installing paru ==="
+  git clone https://aur.archlinux.org/paru.git /tmp/paru
+  cd /tmp/paru
+  makepkg -si --noconfirm
+  cd -
+  rm -rf /tmp/paru
 fi
 
+# install aur packages with paru
+echo -e "\n=== installing aur packages ==="
+paru -S --noconfirm vesktop spotify music-presence-bin surfshark-client
 
-# --- aur packages ---
-echo "--- installing aur packages with paru ---"
-
-aur_packages=(
-    vesktop
-    spotify
-    music-presence-bin
-    surfshark-client
-)
-
-paru -S --needed --noconfirm "${aur_packages[@]}"
-
-
-# --- configure grub ---
-echo "--- configuring grub for os-prober ---"
+# configure grub
+echo -e "\n=== configuring grub ==="
 sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+# set fish as shell
+echo -e "\n=== setting fish as default shell ==="
+chsh -s "$(which fish)"
 
-# --- change default shell to zsh ---
-echo "--- setting zsh as default shell ---"
-# this will ask for your password
-chsh -s "$(which zsh)"
-
-
-# --- configure zsh ---
-echo "--- configuring .zshrc ---"
-
-# create .zshrc if it doesn't exist
-touch ~/.zshrc
-
-# append settings to .zshrc
-cat <<EOF >> ~/.zshrc
-
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-if [[ $- == *i* ]]; then
-  fastfetch
-fi
+# configure fish
+echo -e "\n=== configuring fish ==="
+cat > ~/.config/fish/config.fish << 'EOF'
+if status is-interactive
+    fastfetch
+end
 
 alias fetch='fastfetch'
+
+set fish_greeting
 EOF
 
-
-echo "--- all done. ---"
-echo "run 'sudo shutdown now' to shutdown"
+echo -e "\n=== setup complete! ==="
